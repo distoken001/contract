70cc749909d04c06d0bc927c9a386aac0b2ba109
SouvernirLiu <songnian.liu@outlook.com>
2023-04-12 11:42:01 +0800

    1

--- scripts/opMonitor.ts
+++ scripts/opMonitor.ts
@@ -14,20 +14,36 @@ async function insert( _event_name: string,
   try {
   // 插入数据
   const insertData = {
-  id:null,
-  event_name:_event_name,
-  operater:_operater,
-  order_id:_order_id,
-  data:_data,
-  status:_status,
-  create_time:new Date(),
-  update_time:new Date(),
-  updater:'system',
-  creator:'system',
-  hash:_hash
+    id: null,
+    event_name: _event_name,
+    operater: _operater,
+    order_id: _order_id,
+    data: _data,
+    status: _status,
+    create_time: new Date(),
+    update_time: new Date(),
+    updater: 'system',
+    creator: 'system',
+    hash: _hash
   };
-  console.error('Insert Data Error:', insertData);
-  const [rows, fields] = await connection.execute('INSERT INTO event_logs SET ?', insertData);
+  console.error('Insert Data :', insertData);
+  const query = 'INSERT INTO event_logs SET id=?, event_name=?, operater=?, order_id=?, data=?, status=?, create_time=?, update_time=?, updater=?, creator=?, hash=?';
+
+  const values = [
+    insertData.id,
+    insertData.event_name,
+    insertData.operater,
+    insertData.order_id,
+    insertData.data,
+    insertData.status,
+    insertData.create_time,
+    insertData.update_time,
+    insertData.updater,
+    insertData.creator,
+    insertData.hash
+  ];
+
+  const [rows, fields] = await connection.execute(query, values);
   } catch (error) {
     console.error('Error:', error);
   } finally {
