ce2e6dcc604e361f09d247f48a8cf98787fbc1a7
SouvernirLiu <songnian.liu@outlook.com>
2023-04-14 15:02:44 +0800

    Update op_config.ts

--- scripts/op_config.ts
+++ scripts/op_config.ts
@@ -1,3 +1,3 @@
-const contractAddressCommon = "0x656F9263C86a06C11b2AB869B4E954aC6FA29749";
+const contractAddressCommon = "0xEa932361A73200afCa66cC1215D7889A19A7d0d4";
 const monitorWss="wss://opt-goerli.g.alchemy.com/v2/sWBj_XQ2JLxUoY6emqWwigmtPa2roOXJ";
 export { contractAddressCommon ,monitorWss};
