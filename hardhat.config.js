b2078052337a74dc6ee56d51c700f48a878b06cf
SouvernirLiu <songnian.liu@outlook.com>
2023-04-14 11:55:53 +0800

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 
 
-const GOERLI_API_URL = "https://eth-goerli.g.alchemy.com/v2/auzz3Ba0_bxwxMl1JtC3UTeSs0EGo9S9";
+const OP_GOERLI_API_URL = "https://opt-goerli.g.alchemy.com/v2/sWBj_XQ2JLxUoY6emqWwigmtPa2roOXJ";
 const PRIVATE_KEY = "812eeace1ca352d25a40090732d3b3d2253b36df8b9d493028f954a0bae969b0";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    goerli: {
-      url: `${GOERLI_API_URL}`,
+    op_goerli: {
+      url: `${OP_GOERLI_API_URL}`,
       accounts: [`${PRIVATE_KEY}`]
     }
   },
