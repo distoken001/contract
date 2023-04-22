1450e36414b5f56b9bcb68c31b2e1c1d799dc330
SouvernirLiu <songnian.liu@outlook.com>
2023-04-22 18:07:15 +0800

    新增

--- scripts/arb_chain/op_config.ts
+++ scripts/arb_chain/op_config.ts
@@ -1,25 +1,10 @@
 import { ethers } from "ethers";
 const contractABI = require("../../artifacts/contracts/Ebay.sol/Ebay.json").abi;
-let opContractAddress: string = "";
-let opMonitorWss: string = "";
-let opMonitorHttps: string = "";
-let dbConfig = {};
-if (process.env.NODE_ENV == "dev") {
-  opContractAddress = "0xD0C5B6365268e5A429688c8Ae45E36e9Ac2d2a4e";
-  opMonitorWss =
-    "wss://arb-goerli.g.alchemy.com/v2/3Z1zdRFsUjYSxzBIjSr6Svkej1E4YbNf";
-  opMonitorHttps =
-    "https://arb-goerli.g.alchemy.com/v2/3Z1zdRFsUjYSxzBIjSr6Svkej1E4YbNf";
-  dbConfig = {
-    host: "97.74.86.12",
-    port: 3306,
-    database: "ebay",
-    user: "dev",
-    password: "Dev@1234",
-  };
-} else {
-}
-
+let opContractAddress:string = process.env.OP_CONTRACT_ADDRESS!;
+let opMonitorWss: string =  process.env.API_WSS_ARB!;
+let opMonitorHttps: string =  process.env.API_HTTP_ARB!;
+// 将 JSON 字符串解析为对象
+const dbConfig = JSON.parse( process.env.DB_CONFIG!);
 const opProviderWss = new ethers.providers.WebSocketProvider(opMonitorWss);
 const opProviderHttps = new ethers.providers.StaticJsonRpcProvider(
   opMonitorHttps
--- scripts/op_chain/op_config.ts
+++ scripts/op_chain/op_config.ts
@@ -1,21 +1,10 @@
 import { ethers } from "ethers";
 const contractABI = require("../../artifacts/contracts/Ebay.sol/Ebay.json").abi;
-let opContractAddress = process.env.OP_CONTRACT_ADDRESS;
-let opMonitorWss: string = "";
-let opMonitorHttps: string = "";
-let dbConfig = {};
-  opMonitorWss =
-    "wss://opt-goerli.g.alchemy.com/v2/sWBj_XQ2JLxUoY6emqWwigmtPa2roOXJ";
-  opMonitorHttps =
-    "https://opt-goerli.g.alchemy.com/v2/sWBj_XQ2JLxUoY6emqWwigmtPa2roOXJ";
-  dbConfig = {
-    host: "97.74.86.12",
-    port: 3306,
-    database: "ebay",
-    user: "dev",
-    password: "Dev@1234",
-  };
-
+let opContractAddress:string = process.env.OP_CONTRACT_ADDRESS!;
+let opMonitorWss: string =  process.env.API_WSS_OP!;
+let opMonitorHttps: string =  process.env.API_HTTP_OP!;
+// 将 JSON 字符串解析为对象
+const dbConfig = JSON.parse( process.env.DB_CONFIG!);
 const opProviderWss = new ethers.providers.WebSocketProvider(opMonitorWss);
 const opProviderHttps = new ethers.providers.StaticJsonRpcProvider(
   opMonitorHttps
