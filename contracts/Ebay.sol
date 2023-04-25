1305ba2963015ce78820438391edf218110e3bdf
SouvernirLiu <songnian.liu@outlook.com>
2023-04-25 11:13:14 +0800

    增加争议订单被确认

--- contracts/Ebay.sol
+++ contracts/Ebay.sol
@@ -467,6 +467,7 @@ contract Ebay is Ownable {
         //2、默认争议订单被确认
         //更新状态
         order.status = Status.Completed;
+        /*
         //4、计算双方需要支付的服务费，进行退押金操作
         uint256 sellerFee = order.price.mul(order.amount).mul(sellerRate).div(
             10000
@@ -486,10 +487,24 @@ contract Ebay is Ownable {
             .buyer_pledge
             .sub(order.price.mul(order.amount))
             .sub(buyerFee); //返还买家数量
-
+*/
+        (
+            uint256 sellerFee,
+            uint256 buyerFee,
+            uint256 sellerBack,
+            uint256 buyerBack
+        ) = EbayLib.confirmCalculateRefunds(
+                order.seller_pledge,
+                order.buyer_pledge,
+                order.price,
+                order.amount,
+                buyerRate,
+                sellerRate,
+                order.buyer_ex
+            );
         order.token.safeTransfer(order.seller, sellerBack); //转给卖家
         order.token.safeTransfer(order.buyer, buyerBack); //转给买家
-        order.token.safeTransfer(lockAddr, sellerFee.add(buyerFee)); //fee
+        order.token.safeTransfer(lockAddr, sellerFee.add(buyerFee)); 
         dateTime[_orderId].finishedTimestamp = block.timestamp;
         total[address(order.token)] -= order.buyer_pledge + order.seller_pledge; //更新总质押代币数量
         dateTime[_orderId].adminConfirmTimestamp = block.timestamp;
