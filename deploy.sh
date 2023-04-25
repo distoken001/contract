7a6c030538dbdea49ee0bb7c0d4ce911ac9996c5
SouvernirLiu <songnian.liu@outlook.com>
2023-04-26 00:34:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 安装生产环境依赖
+echo "Installing production dependencies..."
+npm install --production
+
 # 清理项目
 echo "Cleaning the project..."
 npm run clean
@@ -8,9 +12,6 @@ npm run clean
 echo "Compiling the project..."
 npx hardhat compile
 
-# 安装生产环境依赖
-echo "Installing production dependencies..."
-npm install --production
 
 # 使用 TypeScript 编译器编译项目
 echo "Compiling TypeScript..."
