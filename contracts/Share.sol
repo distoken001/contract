74b278ee8d5a0bb899ccdde7d3b044f5ceb90009
SouvernirLiu <songnian.liu@outlook.com>
2023-04-27 18:31:21 +0800

    分红合约

--- contracts/Share.sol
+++ contracts/Share.sol
@@ -5,10 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
-contract Share is PaymentSplitter, Ownable {
-    //松年45 、智超20、金波10 、瑞刚10、洪建5、王超5、亚雄5
-    address[] public _init_payees =[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB];
-    uint256[] public _init_shares =[20,30,50];
+contract SharePayment is PaymentSplitter, Ownable {
+    //xiaolaohu、buqun、bigg、goujian、wangchao、jinbo
+    address[] public _init_payees =[0xc69eC797d7e38A70Bcc9D2a5Ca1A3A64631AAe1C,0xb1B8a8E9c2FFcc0B2072937d170bAe4E794f6238,0xc93abD3762aed8E9823c787C542eD7Aede555e3C,0x44207d32b2b7af723d6035c069bC49DBEec589FF,0x86620EB5C1feb9b8c80411911f99863Fa5bF34D6,0x7568F2aD4b539e49e3A4f586867E13418EFc9541];
+    uint256[] public _init_shares =[15,55,10,5,5,10];
+
     constructor() PaymentSplitter(_init_payees,_init_shares) {
     }
+
 }
