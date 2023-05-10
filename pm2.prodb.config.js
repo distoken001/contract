4e6bf66faf5058e9ad142be68bca3110de6a327a
SouvernirLiu <songnian.liu@outlook.com>
2023-05-10 14:05:13 +0800

    添加

---
+++ pm2.prodb.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+    apps: [
+      {
+        name: 'monitor_op',
+        script: ' node ./dist/op_chain/start.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '512M',
+        env: {
+          NODE_ENV: 'productionb',
+        }
+      },
+      // {
+      //   name: 'monitor_arb',
+      //   script: 'node ./dist/arb_chain/start.js',
+      //   instances: 1,
+      //   autorestart: true,
+      //   watch: false,
+      //   max_memory_restart: '512M',
+      //   env: {
+      //     NODE_ENV: 'production',
+      //   }
+      // },
+      // {
+      //   name: 'monitor_main',
+      //   script: 'node ./dist/main_chain/start.js',
+      //   instances: 1,
+      //   autorestart: true,
+      //   watch: false,
+      //   max_memory_restart: '512M',
+      //   env: {
+      //     NODE_ENV: 'production',
+      //   }
+      // },
+      {
+        name: 'monitor_polygon',
+        script: 'node ./dist/polygon_chain/start.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '512M',
+        env: {
+          NODE_ENV: 'productionb',
+        }
+      },
+      // {
+      //   name: 'monitor_bsc',
+      //   script: 'node ./dist/bsc_chain/start.js',
+      //   instances: 1,
+      //   autorestart: true,
+      //   watch: false,
+      //   max_memory_restart: '512M',
+      //   env: {
+      //     NODE_ENV: 'production',
+      //   }
+      // }
+    ]
+  };
--- scripts/polygon_chain/config.ts
+++ scripts/polygon_chain/config.ts
@@ -1,26 +1,25 @@
 import { ethers } from "ethers";
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: '.env.production' });
-} else if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: '.env.development' });
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: ".env.development" });
+} else if (process.env.NODE_ENV === "productionb") {
+  dotenv.config({ path: ".env.productionb" });
 }
 const contractABI = require("../../artifacts/contracts/Ebay.sol/Ebay.json").abi;
-const tokenContractABI = require("../../artifacts/contracts/Token.sol/Token.json").abi;
+const tokenContractABI =
+  require("../../artifacts/contracts/Token.sol/Token.json").abi;
 let contractAddress: string = process.env.POLYGON_CONTRACT_ADDRESS!;
 let monitorWss: string = process.env.API_WSS_POLYGON!;
 let monitorHttps: string = process.env.API_HTTP_POLYGON!;
 // 将 JSON 字符串解析为对象
 const dbConfig = JSON.parse(process.env.DB_CONFIG!);
 const providerWss = new ethers.providers.WebSocketProvider(monitorWss);
-const providerHttps = new ethers.providers.StaticJsonRpcProvider(
-  monitorHttps
-);
+const providerHttps = new ethers.providers.StaticJsonRpcProvider(monitorHttps);
 const iface = new ethers.utils.Interface(contractABI);
-const chainId = providerHttps
-  .getNetwork()
-  .then((network) => network.chainId);
+const chainId = providerHttps.getNetwork().then((network) => network.chainId);
 const contract = new ethers.Contract(
   contractAddress,
   contractABI,
@@ -35,5 +34,5 @@ export {
   chainId,
   contract,
   iface,
-  tokenContractABI
+  tokenContractABI,
 };
