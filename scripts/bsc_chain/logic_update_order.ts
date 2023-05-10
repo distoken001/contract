39f41ac849a3be978dc13814f443dab86d66ea9c
SouvernirLiu <songnian.liu@outlook.com>
2023-05-10 16:35:21 +0800

    修改

--- scripts/bsc_chain/logic_update_order.ts
+++ scripts/bsc_chain/logic_update_order.ts
@@ -1,13 +1,16 @@
 import { Status } from "./enum_all";
 import { executeQuery } from "./db";
 
-export async function UpdateStatus(
+export async function logic_update_order(
   _order_id: number,
   _status: Status,
   _buyer: string,
   _buyer_ex:number,
   _buyer_pledge:number,
-  _chain_id :number
+  _amount:number,
+  _seller_pledge:number,
+  _chain_id :number,
+  _contract_address:string
 ) {
   const updateData = {
     order_id: _order_id,
@@ -18,10 +21,13 @@ export async function UpdateStatus(
     buyer: _buyer,
     chain_id:_chain_id,
     buyer_ex:_buyer_ex,
-    buyer_pledge:_buyer_pledge
+    buyer_pledge:_buyer_pledge,
+    seller_pledge:_seller_pledge,
+    amount:_amount,
+    contract_address:_contract_address
   };
   const query =
-    "update orders SET status=?,buyer_ex=?, update_time=?,buyer= ?,buyer_pledge= ?  where order_id=? and chain_id=?";
+    "update orders SET status=?,buyer_ex=?, update_time=?,buyer= ?,buyer_pledge= ?,seller_pledge=?,amount=?  where order_id=? and chain_id=? and contract=?";
 
   const values = [
     updateData.status,
@@ -29,8 +35,11 @@ export async function UpdateStatus(
     updateData.update_time,
     updateData.buyer,
     updateData.buyer_pledge,
+    updateData.seller_pledge,
+    updateData.amount,
     updateData.order_id,
-    updateData.chain_id
+    updateData.chain_id,
+    updateData.contract_address
   ];
 
   const [rows, fields] = await executeQuery(query, values);
