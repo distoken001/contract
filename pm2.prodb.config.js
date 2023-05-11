0b94184c949783fadc429d8a5c85307e24185185
SouvernirLiu <songnian.liu@outlook.com>
2023-05-11 10:19:57 +0800

    Update pm2.prodb.config.js

--- pm2.prodb.config.js
+++ pm2.prodb.config.js
@@ -22,17 +22,17 @@ module.exports = {
           NODE_ENV: 'productionb',
         }
       },
-      {
-        name: 'monitor_main',
-        script: 'node ./dist/main_chain/start.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '512M',
-        env: {
-          NODE_ENV: 'productionb',
-        }
-      },
+      // {
+      //   name: 'monitor_main',
+      //   script: 'node ./dist/main_chain/start.js',
+      //   instances: 1,
+      //   autorestart: true,
+      //   watch: false,
+      //   max_memory_restart: '512M',
+      //   env: {
+      //     NODE_ENV: 'productionb',
+      //   }
+      // },
       {
         name: 'monitor_polygon',
         script: 'node ./dist/polygon_chain/start.js',
@@ -44,16 +44,16 @@ module.exports = {
           NODE_ENV: 'productionb',
         }
       },
-      {
-        name: 'monitor_bsc',
-        script: 'node ./dist/bsc_chain/start.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '512M',
-        env: {
-          NODE_ENV: 'productionb',
-        }
-      }
+      // {
+      //   name: 'monitor_bsc',
+      //   script: 'node ./dist/bsc_chain/start.js',
+      //   instances: 1,
+      //   autorestart: true,
+      //   watch: false,
+      //   max_memory_restart: '512M',
+      //   env: {
+      //     NODE_ENV: 'productionb',
+      //   }
+      // }
     ]
   };
