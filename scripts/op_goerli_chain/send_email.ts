dc3a966a80b4ffe78e610cfbe603d27de489e77a
SouvernirLiu <songnian.liu@outlook.com>
2023-05-18 18:26:12 +0800

    1

--- scripts/op_goerli_chain/send_email.ts
+++ scripts/op_goerli_chain/send_email.ts
@@ -1,17 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function sendemail(chain_id:number,contract:string,order_id:number) {
-  try {
-    const url = 'https://api-dev.de-market.com/api/notice/sendemail';
-    const data = {
-      chain_id: chain_id,
-      contract: contract,
-      order_id: order_id,
-    };
+export async function sendemail(
+  chain_id: number,
+  contract: string,
+  order_id: number
+) {
+  if (process.env.NODE_ENV == "production"||process.env.NODE_ENV == "developmnent") {
+    try {
+      const url = "https://api.de-market.com/api/notice/sendemail";
+      const data = {
+        chain_id: chain_id,
+        contract: contract,
+        order_id: order_id,
+      };
 
-    const response = await axios.post(url, data);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+      const response = await axios.post(url, data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
