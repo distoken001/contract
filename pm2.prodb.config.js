4e8562fe33eb1acb53260d997fd73b0e9c393adc
SouvernirLiu <songnian.liu@outlook.com>
2023-05-23 14:10:29 +0800

    修改

--- pm2.prodb.config.js
+++ pm2.prodb.config.js
@@ -44,17 +44,17 @@ module.exports = {
         NODE_ENV: "productionb",
       },
     },
-    // {
-    //   name: 'monitor_bsc',
-    //   script: 'node ./dist/bsc_chain/start.js',
-    //   instances: 1,
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: '512M',
-    //   env: {
-    //     NODE_ENV: 'productionb',
-    //   }
-    // },
+    {
+      name: 'monitor_bsc',
+      script: 'node ./dist/bsc_chain/start.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M',
+      env: {
+        NODE_ENV: 'productionb',
+      }
+    },
     // {
     //   name: 'monitor_main',
     //   script: 'node ./dist/main_chain/start.js',
