dc031b1cf34222a9bf5bd087349c20d987c9004c
SouvernirLiu <songnian.liu@outlook.com>
2023-06-25 10:41:26 +0800

    Update pm2.prod.config.js

--- pm2.prod.config.js
+++ pm2.prod.config.js
@@ -44,18 +44,6 @@ module.exports = {
         NODE_ENV: "production",
       },
     },
-    // {
-    //   name: 'monitor_main',
-    //   script: 'node ./dist/main_chain/start.js',
-    //   instances: 1,
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: '512M',
-    //   env: {
-    //     NODE_ENV: 'production',
-    //   }
-    // },
-
     {
       name: "monitor_bsc",
       script: "node ./dist/master/v1/bsc_chain/start.js",
@@ -68,8 +56,63 @@ module.exports = {
       },
     },
     {
-      name: "monitor_conflux",
+      name: "v2_monitor_conflux",
       script: "node ./dist/master/v2/conflux_chain/start.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "v2_monitor_op",
+      script: "node ./dist/master/v2/op_chain/start.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "v2_monitor_arb",
+      script: "node ./dist/master/v2/arb_chain/start.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "v2_monitor_polygon",
+      script: "node ./dist/master/v2/polygon_chain/start.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "v2_monitor_avax",
+      script: "node ./dist/master/v2/avax_chain/start.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "v2_monitor_bsc",
+      script: "node ./dist/master/v2/bsc_chain/start.js",
       instances: 1,
       autorestart: true,
       watch: false,
