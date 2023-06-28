f5f62e09b6314797ca0a517d6b3ba8e86cd6dcfb
SouvernirLiu <songnian.liu@outlook.com>
2023-06-28 18:11:38 +0800

    Update pm2.dev.config.js

--- pm2.dev.config.js
+++ pm2.dev.config.js
@@ -6,6 +6,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
+        cron_restart: '0 0 * * *', // 每天午夜重启一次
         max_memory_restart: '512M',
         env: {
           NODE_ENV: 'development',
@@ -17,6 +18,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
+        cron_restart: '0 0 * * *', // 每天午夜重启一次
         max_memory_restart: '512M',
         env: {
           NODE_ENV: 'development',
@@ -28,6 +30,7 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
+        cron_restart: '0 0 * * *', // 每天午夜重启一次
         max_memory_restart: '512M',
         env: {
           NODE_ENV: 'development',
