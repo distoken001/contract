9504418a48d7934f3a0ad5cee1a07a3725b9e7ab
SouvernirLiu <songnian.liu@outlook.com>
2023-07-27 17:49:31 +0800

    大修改

--- pm2.ebay.dev.config.js
+++ pm2.ebay.dev.config.js
@@ -13,18 +13,6 @@ module.exports = {
         }
       },
       {
-        name: 'ebay_arb_goerli',
-        script: ' node ./dist/ebay/dev/v1/arb_goerli_chain/start.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        cron_restart: '0 0 * * *', // 每天午夜重启一次
-        max_memory_restart: '512M',
-        env: {
-          NODE_ENV: 'development',
-        }
-      },
-      {
         name: 'ebay_v2_op_goerli',
         script: ' node ./dist/ebay/dev/v2/op_goerli_chain/start.js',
         instances: 1,
