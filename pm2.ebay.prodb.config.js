545b305cc959ef1e106a58a109bdace5639bf068
SouvernirLiu <songnian.liu@outlook.com>
2023-08-31 09:27:08 +0800

    Update pm2.ebay.prodb.config.js

--- pm2.ebay.prodb.config.js
+++ pm2.ebay.prodb.config.js
@@ -48,54 +48,54 @@ module.exports = {
         NODE_ENV: "productionb",
       },
     },
-    {
-      name: "ebay_v2_op_prodb",
-      script: "node ./dist/ebay/master/v2/op_chain/start.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      cron_restart: '0 0 * * *', // 每天午夜重启一次
-      max_memory_restart: "512M",
-      env: {
-        NODE_ENV: "productionb",
-      },
-    },
-    {
-      name: "ebay_v2_arb_prodb",
-      script: "node ./dist/ebay/master/v2/arb_chain/start.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      cron_restart: '0 0 * * *', // 每天午夜重启一次
-      max_memory_restart: "512M",
-      env: {
-        NODE_ENV: "productionb",
-      },
-    },
-    {
-      name: "ebay_v2_polygon_prodb",
-      script: "node ./dist/ebay/master/v2/polygon_chain/start.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      cron_restart: '0 0 * * *', // 每天午夜重启一次
-      max_memory_restart: "512M",
-      env: {
-        NODE_ENV: "productionb",
-      },
-    },
-    {
-      name: "ebay_v2_bsc_prodb",
-      script: "node ./dist/ebay/master/v2/bsc_chain/start.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      cron_restart: '0 0 * * *', // 每天午夜重启一次
-      max_memory_restart: "512M",
-      env: {
-        NODE_ENV: "productionb",
-      },
-    },
+    // {
+    //   name: "ebay_v2_op_prodb",
+    //   script: "node ./dist/ebay/master/v2/op_chain/start.js",
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   cron_restart: '0 0 * * *', // 每天午夜重启一次
+    //   max_memory_restart: "512M",
+    //   env: {
+    //     NODE_ENV: "productionb",
+    //   },
+    // },
+    // {
+    //   name: "ebay_v2_arb_prodb",
+    //   script: "node ./dist/ebay/master/v2/arb_chain/start.js",
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   cron_restart: '0 0 * * *', // 每天午夜重启一次
+    //   max_memory_restart: "512M",
+    //   env: {
+    //     NODE_ENV: "productionb",
+    //   },
+    // },
+    // {
+    //   name: "ebay_v2_polygon_prodb",
+    //   script: "node ./dist/ebay/master/v2/polygon_chain/start.js",
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   cron_restart: '0 0 * * *', // 每天午夜重启一次
+    //   max_memory_restart: "512M",
+    //   env: {
+    //     NODE_ENV: "productionb",
+    //   },
+    // },
+    // {
+    //   name: "ebay_v2_bsc_prodb",
+    //   script: "node ./dist/ebay/master/v2/bsc_chain/start.js",
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   cron_restart: '0 0 * * *', // 每天午夜重启一次
+    //   max_memory_restart: "512M",
+    //   env: {
+    //     NODE_ENV: "productionb",
+    //   },
+    // },
     // {
     //   name: "ebay_v2_conflux_prodb",
     //   script: "node ./dist/ebay/master/v2/conflux_chain/start.js",
