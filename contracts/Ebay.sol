a35b7361ee6d26f3cf54691422b20c9ff1206481
SouvernirLiu <songnian.liu@outlook.com>
2023-09-06 19:01:49 +0800

    1

--- contracts/Ebay.sol
+++ contracts/Ebay.sol
@@ -248,6 +248,13 @@ contract Ebay is Ownable {
                 order.seller,
                 order.buyer
             );
+             emit AddOrder(
+                _user,
+                _order_id_new,
+                Status.Ordered,
+                order.seller,
+                _user
+            );
         }
         order.token.transferFrom(_user, address(this), _buyer_pledge);
         total[address(order.token)] = total[address(order.token)].add(
