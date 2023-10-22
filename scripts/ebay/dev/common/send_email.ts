b61edd8bb7bffff121f079796f23d26fd92e4790
SouvernirLiu <songnian.liu@outlook.com>
2023-10-22 16:39:32 +0800

    1

--- scripts/ebay/dev/common/send_email.ts
+++ scripts/ebay/dev/common/send_email.ts
@@ -33,7 +33,7 @@ export async function notice_bot(chain_id: number, name: string, user: string) {
       " https://api.telegram.org/bot6753397970:AAGd0t3JyjNyvthf7pO14nL6R8zC_vIW1I0/sendMessage?chat_id=-1002144169559&text=";
     url =
       url +
-      "拍卖订单: 用户 " +
+      "市场订单: 用户 " +
       user +
       "在" +
       a.toString() +
@@ -47,7 +47,7 @@ export async function notice_bot(chain_id: number, name: string, user: string) {
       " https://api.telegram.org/bot6753397970:AAGd0t3JyjNyvthf7pO14nL6R8zC_vIW1I0/sendMessage?chat_id=-1001814533790&text=";
     url =
       url +
-      "拍卖订单: 用户 " +
+      "市场订单: 用户 " +
       user +
       "在" +
       a.toString() +
