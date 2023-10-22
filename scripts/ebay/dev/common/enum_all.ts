ce954cd5b08611203c47c5a1edbd17f4c53ee6df
SouvernirLiu <songnian.liu@outlook.com>
2023-10-22 11:23:39 +0800

    增加机器人推送

--- scripts/ebay/dev/common/enum_all.ts
+++ scripts/ebay/dev/common/enum_all.ts
@@ -11,3 +11,38 @@ export enum Status{
     BuyerRejectCancel,//买家拒绝取消9
     ConsultCancelCompleted//协商取消完成10
   }    
+  export enum ChainEnum 
+  {
+      Ethereum = 1,
+      Arbitrum = 42161,
+      Optimism = 10,
+      Polygon = 137,
+      Ropsten = 3,
+      Rinkeby = 4,
+      Goerli = 5,
+      Kovan = 42,
+      Bsc = 56,
+      Sokol = 77,
+      BscTestnet = 97,
+      Xdai = 100,
+      Heco = 100,
+      Opera = 250,
+      HecoTestnet = 256,
+      OptimisticGoerli = 420,
+      Moonbeam = 1284,
+      Moonriver = 1285,
+      MoonbaseAlpha = 1287,
+      FtmTestnet = 4002,
+      Chiado = 10200,
+      AvalancheFujiTestnet = 43113,
+      Avalanche = 43114,
+      PolygonMumbai = 80001,
+      ArbitrumTestnet = 421613,
+      ArbitrumGoerli = 421613,
+      Aepolia = 11155111,
+      Aurora = 1313161554,
+      AuroraTestnet = 1313161555,
+      Harmony = 1666600000,
+      HarmonyTest = 1666700000,
+      Conflux = 1030
+  }
